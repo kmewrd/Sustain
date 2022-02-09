6c5379754e4230c0074e2d65ec108597d27bd02c
Kim Ward <kmewrd@outlook.com>
2022-02-09 16:51:25 -0600

    Add method that gets average step goal among all users

--- src/classes/UserRepository.js
+++ src/classes/UserRepository.js
@@ -4,6 +4,13 @@ class UserRepository {
   constructor(data) {
     this.users = data;
   }
+  getAvgUserStepGoal() {
+    const total = this.users.reduce((acc, user) => {
+      return acc += user.dailyStepGoal;
+    }, 0);
+    const avg = total / this.users.length;
+    return avg;
+  }
 }
 
 export default UserRepository;
