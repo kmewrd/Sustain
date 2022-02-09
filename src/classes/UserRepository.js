bdb7ea5a89ece2f5206a0ed5e38962c3f94bf77f
Kim Ward <kmewrd@outlook.com>
2022-02-09 11:44:01 -0600

    Refactor createAllUsers method

--- src/classes/UserRepository.js
+++ src/classes/UserRepository.js
@@ -3,8 +3,7 @@ class UserRepository {
     this.users = data;
   }
   createAllUsers() {
-		const userInstances = this.users.map(user => new User(user));
-    this.users = userInstances;
+		this.users = this.users.map(user => new User(user));
 	}
 }
 
