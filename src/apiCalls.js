b1686472ca4f9e230049171a79f4190b396e6050
Kevin King <kevinturingking@gmail.com>
2022-02-09 17:50:28 -0700

    Write and export function that fetches user data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,11 @@
 // Your fetch requests will live here!
+const fetchUserData = () => {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/users").then(response => response.json()).then(data => data.userData);
+}
+
+
+export {fetchUserData};
+
 
 
 console.log('I will be a fetch request!')
-
