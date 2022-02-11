e710cfad6cd2b927fb086f939700ec0f94ad36b0
Casey Halstead <catherineclosee@gmail.com>
2022-02-11 15:01:24 -0600

    Create getAvgHoursSLeepPerDay method on SleepLog

--- src/classes/SleepLog.js
+++ src/classes/SleepLog.js
@@ -5,6 +5,13 @@ class SleepLog {
     this.hoursSlept = data.hoursSlept;
     this.sleepQuality = data.sleepQuality;
   }
+  getAvgHoursSleepPerDay() {
+    const total = this.sleepLogs.reduce((acc, log) => {
+      return acc += log.hoursSlept;
+    }, 0);
+    const avg = total / this.sleepLogs.length;
+    return avg;
+  };
 }
 
 export default SleepLog;
