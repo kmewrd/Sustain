f4c8d0d048a941b084e80c6cbeeb6fe8fd9f072c
Katie Ammon <katie.ammon10@gmail.com>
2022-02-12 16:41:21 -0700

    Add sleepLog-test

---
+++ test/sleepLog-test.js
@@ -0,0 +1,37 @@
+import { expect } from 'chai';
+import SleepLog from '../src/classes/SleepLog';
+
+describe('Sleep', () => {
+
+let sleepLog;
+let data;
+beforeEach(() => {
+ data = {
+   "userID":1,
+   "date":"2019/06/15",
+   "hoursSlept":6.1,
+   "sleepQuality":2.2
+ }
+  sleepLog = new SleepLog(data)
+})
+
+it(' should be a function', () => {
+  expect(SleepLog).to.be.a('function')
+});
+
+it('should be an instance of SleepLog', () => {
+  expect(sleepLog).to.be.an.instanceof(SleepLog)
+});
+
+it('should have a property of userID', () => {
+  expect(sleepLog.userID).to.equal(1)
+});
+
+it('should have a property of date', () => {
+     expect(sleepLog.date).to.equal("2019/06/15")
+});
+
+it('should have a property of hoursSlept', () => {
+  expect(sleepLog.hoursSlept).to.equal(6.1)
+});
+})
