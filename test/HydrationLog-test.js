aaab1bbe1d48dde4808033e9c9f5971bf29b2f67
Katie Ammon <katie.ammon10@gmail.com>
2022-02-12 16:36:06 -0700

    Add test to check for numOunces

--- test/HydrationLog-test.js
+++ test/HydrationLog-test.js
@@ -29,10 +29,9 @@ it('should have a property of userID', () => {
 
 it('should have a property of date', () => {
      expect(hydrationLog.date).to.equal("2019/06/15")
-})
+});
 
-
-
-
-//  should have a property of numOunces
+it('should have a property of numOunces', () => {
+  expect(hydrationLog.numOunces).to.equal(37)
+});
 })
