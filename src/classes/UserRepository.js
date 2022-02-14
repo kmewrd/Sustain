d8e3ed3c0214dcc8fa896bf8be8c8b01e7f42902
Katie Ammon <katie.ammon10@gmail.com>
2022-02-14 14:37:41 -0700

    refactor UserRepo

--- src/classes/UserRepository.js
+++ src/classes/UserRepository.js
@@ -3,7 +3,8 @@ class UserRepository {
     this.users = userData;
     this.hydrationLogs = hydrationData;
     this.sleepLogs = sleepData;
-  }
+  };
+
   getUserById(id) {
     const user = this.users.find(person => {
       return person.id === id;
@@ -11,27 +12,30 @@ class UserRepository {
     user.hydrationLogs = this.getUserLogs(this.hydrationLogs, id);
     user.sleepLogs = this.getUserLogs(this.sleepLogs, id);
     return user;
-  }
+  };
+
   getAvgUserStepGoal() {
     const total = this.users.reduce((acc, user) => {
       return acc += user.dailyStepGoal;
     }, 0);
     const avg = total / this.users.length;
     return avg;
-  }
+  };
+
   getUserLogs(dataLogs, id) {
     const userLogs = dataLogs.filter(log => {
       return log.userID === id;
     });
     return userLogs;
-  }
+  };
+
   getAvgUserSleepQuality() {
     const total = this.sleepLogs.reduce((acc, log) => {
       return acc += log.sleepQuality;
     }, 0);
     const avg = total / this.users.length;
     return avg;
-  }
-}
+  };
+};
 
 export default UserRepository;
