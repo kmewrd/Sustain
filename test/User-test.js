122e529d56c20da6a804352ba890b93ec91a8afb
Katie Ammon <katie.ammon10@gmail.com>
2022-02-14 14:54:57 -0700

    Refactor User-test

--- test/User-test.js
+++ test/User-test.js
@@ -1,4 +1,4 @@
- import { expect } from 'chai';
+import { expect } from 'chai';
 import User from '../src/classes/User';
 import UserRepository from '../src/classes/UserRepository';
 
@@ -52,61 +52,61 @@ describe('User', () => {
 
     user1 = new User(testAPIData[0]);
     testUserData = [user1];
-    userRepository = new UserRepository(testUserData, testHydrationData, testSleepData)
+    userRepository = new UserRepository(testUserData, testHydrationData, testSleepData);
     userRepository.getUserById(1);
   });
 
- it('should be a function', () => {
-      expect(User).to.be.a('function')
- });
+  it('should be a function', () => {
+        expect(User).to.be.a('function');
+  });
 
- it('should be an instance of User', () => {
-      expect(user1).to.be.an.instanceof(User)
- });
+  it('should be an instance of User', () => {
+    expect(user1).to.be.an.instanceof(User);
+  });
 
- it('should take in userData', () => {
-      expect(user1.id).to.equal(1);
-      expect(user1.name).to.equal("Luisa Hane");
-      expect(user1.address.length).to.equal(2);
-      expect(user1.email).to.equal("Diana.Hayes1@hotmail.com");
-      expect(user1.strideLength).to.equal(4.3);
-      expect(user1.dailyStepGoal).to.equal(10000);
-      expect(user1.friends.length).to.equal(3); 
- });
+  it('should take in userData', () => {
+    expect(user1.id).to.equal(1);
+    expect(user1.name).to.equal("Luisa Hane");  
+    expect(user1.address.length).to.equal(2);
+    expect(user1.email).to.equal("Diana.Hayes1@hotmail.com");
+    expect(user1.strideLength).to.equal(4.3);
+    expect(user1.dailyStepGoal).to.equal(10000);
+    expect(user1.friends.length).to.equal(3); 
+  });
 
- it('should return first name', () => {
-      expect(user1.returnFirstName()).to.equal("Luisa")
- });
+  it('should return first name', () => {
+    expect(user1.returnFirstName()).to.equal("Luisa");
+  });
 
- it('should have a method that returns avg ounces per day', () => {
-   expect(user1.getAvgOuncesPerDay()).to.equal(22)
- });
+  it('should have a method that returns avg ounces per day', () => {
+    expect(user1.getAvgOuncesPerDay()).to.equal(22);
+  });
 
- it('should get ounces by week', () => {
-   expect(user1.getOuncesByWeek("2021/05/14")).to.deep.equal([32])
- });
+  it('should get ounces by week', () => {
+    expect(user1.getOuncesByWeek("2021/05/14")).to.deep.equal([32]);
+  });
 
- it('should get avg hours slept per day', () => {
-   expect(user1.getAvgHoursSleepPerDay()).to.equal('7.0')
- });
+  it('should get avg hours slept per day', () => {
+    expect(user1.getAvgHoursSleepPerDay()).to.equal('7.0');
+  });
 
- it('should get avg sleep quality per day', () => {
-   expect(user1.getAvgSleepQualityPerDay()).to.equal('4.0')
- });
+  it('should get avg sleep quality per day', () => {
+    expect(user1.getAvgSleepQualityPerDay()).to.equal('4.0');
+  });
 
- it('should get sleep hours by a specific date', () => {
-   expect(user1.getSleepHoursByDay("2020/05/14")).to.equal(8)
- });
+  it('should get sleep hours by a specific date', () => {
+    expect(user1.getSleepHoursByDay("2020/05/14")).to.equal(8);
+  });
 
- it('should get sleep quality by a specific date', () => {
-   expect(user1.getSleepQualityByDay("2020/05/14")).to.equal(6)
- });
+  it('should get sleep quality by a specific date', () => {
+    expect(user1.getSleepQualityByDay("2020/05/14")).to.equal(6);
+  });
 
- it('should get hours slept by week', () => {
-   expect(user1.getHoursSleptByWeek("2020/05/14")).to.deep.equal([6])
- });
+  it('should get hours slept by week', () => {
+    expect(user1.getHoursSleptByWeek("2020/05/14")).to.deep.equal([6]);
+  });
 
- it('should get sleep quality by week', ()=> {
-   expect(user1.getSleepQualityByWeek("2020/05/14")).to.deep.equal([2])
- });
+  it('should get sleep quality by week', ()=> {
+    expect(user1.getSleepQualityByWeek("2020/05/14")).to.deep.equal([2]);
+  });
 });
