c24d854ebb83b92b0f0f14259f3b896a73f642a0
Katie Ammon <katie.ammon10@gmail.com>
2022-02-14 14:48:36 -0700

    Refactor SleepLog-test

--- test/sleepLog-test.js
+++ test/sleepLog-test.js
@@ -3,39 +3,39 @@ import SleepLog from '../src/classes/SleepLog';
 
 describe('SleepLog', () => {
 
-let sleepLog;
-let data;
-beforeEach(() => {
- data = {
-   "userID":1,
-   "date":"2019/06/15",
-   "hoursSlept":6.1,
-   "sleepQuality":2.2
- }
-  sleepLog = new SleepLog(data)
-});
-
-it(' should be a function', () => {
-  expect(SleepLog).to.be.a('function')
-});
-
-it('should be an instance of SleepLog', () => {
-  expect(sleepLog).to.be.an.instanceof(SleepLog)
-});
-
-it('should have a property of userID', () => {
-  expect(sleepLog.userID).to.equal(1)
-});
-
-it('should have a property of date', () => {
-     expect(sleepLog.date).to.equal("2019/06/15")
-});
-
-it('should have a property of hoursSlept', () => {
-  expect(sleepLog.hoursSlept).to.equal(6.1)
-});
-
-it('should have a property of sleepQuality', () => {
-  expect(sleepLog.sleepQuality).to.equal(2.2)
+  let sleepLog;
+  let data;
+  beforeEach(() => {
+  data = {
+    "userID":1,
+    "date":"2019/06/15",
+    "hoursSlept":6.1,
+    "sleepQuality":2.2
+  };
+    sleepLog = new SleepLog(data)
   });
+
+  it(' should be a function', () => {
+    expect(SleepLog).to.be.a('function');
+    });
+
+  it('should be an instance of SleepLog', () => {
+    expect(sleepLog).to.be.an.instanceof(SleepLog);
+  });
+
+  it('should have a property of userID', () => {
+    expect(sleepLog.userID).to.equal(1);
+  });
+
+  it('should have a property of date', () => {
+      expect(sleepLog.date).to.equal("2019/06/15");
+  });
+
+  it('should have a property of hoursSlept', () => {
+    expect(sleepLog.hoursSlept).to.equal(6.1);
+  });
+
+  it('should have a property of sleepQuality', () => {
+    expect(sleepLog.sleepQuality).to.equal(2.2);
+    });
 });
