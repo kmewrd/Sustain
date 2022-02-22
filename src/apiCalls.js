c64d26c582e722b4fb175ab47278e8391b977cbb
Kim Ward <kmewrd@outlook.com>
2022-02-22 17:34:52 -0600

    Combine fetch calls into one fetch request with a parameter

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,20 +1,8 @@
 // Your fetch requests will live here!
-const fetchUserData = () => {
-  return fetch("https://fitlit-api.herokuapp.com/api/v1/users")
+const fetchData = (extension) => {
+  return fetch(`https://fitlit-api.herokuapp.com/api/v1/${extension}`)
     .then(response => response.json())
     .catch(err => console.log(err));
 };
 
-const fetchHydrationData = () => {
-  return fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
-    .then(response => response.json())
-    .catch(err => console.log(err));
-};
-
-const fetchSleepData = () => {
-  return fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
-    .then(response => response.json())
-    .catch(err => console.log(err));
-};
-
-export {fetchUserData, fetchHydrationData, fetchSleepData};
+export {fetchData};
